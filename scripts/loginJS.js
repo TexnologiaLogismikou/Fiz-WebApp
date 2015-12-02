6bfef6f1bebeea34211305a130b2f258dbfb7783
newbi12 <shinigamidxd@gmail.com>
2015-12-02 16:07:10 +0200

    Added login

---
+++ scripts/loginJS.js
@@ -0,0 +1,24 @@
+/**
+ * Created by KuroiTenshi on 12/2/2015.
+ */
+
+function loginUser(){
+    var json = {"username" : document.getElementById("username").value,
+                "password" : document.getElementById("password").value};
+
+    $.ajax({
+        url  : "http://localhost:8080/Fiz/login",
+        type : 'post',
+        crossDomain: true,
+        contentType: "application/json;",
+        data : JSON.stringify(json),
+        success : function(str){
+            alert(str)
+        },
+
+        error : function() {
+            alert("error");
+        }
+    });
+
+}
