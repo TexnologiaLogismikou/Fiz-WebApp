43f6de928bb8741829528b0fb690ad207a9d0764
Alodapos <kazi666@hotmail.gr>
2015-12-10 17:55:43 +0200

    Master : Finished login
    finished login script
    removed obsolete files

--- scripts/cookie.js
+++ scripts/cookie.js
@@ -14,3 +14,12 @@ function getCookie(username) {
 
     return "";
 }
+
+
+function setCookie(name,value,expires,path,domain,secure) {
+    document.cookie = name + "=" + escape(value) +
+        ( (expires) ? ";expires=" + expires.toGMTString() : "") +
+        ( (path) ? ";path=" + path : "") +
+        ( (domain) ? ";domain=" + domain : "") +
+        ( (secure) ? ";secure" : "");
+}
--- scripts/loginJS.js
+++ scripts/loginJS.js
@@ -1,44 +1,26 @@
-/**
- * Created by KuroiTenshi on 12/2/2015.
- */
-
-//var Session = {
-//    id : '${pageContext.session.id}',
-//    user : '${pageContext.request.remoteUser}'
-//};
-
-function loginUser(){
+function loginUser() {
 
     $.ajax({
-        url  : "http://83.212.105.54/Fiz/login",
-        type : 'post',
+        url: "http://83.212.105.54:8080/Fiz/login",
+        type: 'post',
         crossDomain: true,
-        //contentType: "application/json;",
-        //accept: "application/json",
-        data : $("#form").serialize(),
-
-        success : function(a,b,c){
-            document.cookie = a;
-            alert(a.status+" 1 "+ a+" 2 "+b+" 3 "+ c);
+        xhrFields: {
+            withCredentials: true
         },
 
-        error : function(response, b, c) {
-            alert("Error "+response.status+ " "+b+" "+c);
+        data: $("#form").serialize(),
+
+        success: function (response, status, xhr) {
+            alert(JSON.stringify(xhr));
+        },
+
+        error: function (response, b, c) {
+            alert("Error " + response.status + " " + b + " " + c);
         }
     });
-
 }
 
-//function setCookie(name,value,expires,path,domain,secure) {
-//    var cookieString = name + "=" + escape(value) +
-//        ( (expires) ? ";expires=" + expires.toGMTString() : "") +
-//        ( (path) ? ";path=" + path : "") +
-//        ( (domain) ? ";domain=" + domain : "") +
-//        ( (secure) ? ";secure" : "");
-//    document.cookie = cookieString;
-//}
-
-function logout(){
+function logout() {
     var cookies = document.cookie.split(";");
 
     for (var i = 0; i < cookies.length; i++) {
